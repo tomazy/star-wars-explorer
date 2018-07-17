52dad28457c082902061b5a5c16742fbca997d10
Tomek Maszkowski <tomazy@go2.pl>
2018-07-18 05:37:16 +0800

    Default ordering of resources by id

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ApplicationRecord
+  default_scope { order(id: :asc) }
   belongs_to :planet
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,3 +1,4 @@
 class Planet < ApplicationRecord
+  default_scope { order(id: :asc) }
   has_many :residents, class_name: Person.name
 end
